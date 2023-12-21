463d2b3e6571c5ba86645016ec1c0f9778f17604
Nav Saini <navdeep@mailbox.org>
2023-12-21 23:29:03 +0530

    change ai autocompletion binding to <tab>

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -5,7 +5,7 @@ return {
     event = "BufEnter",
     config = function()
       -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set("i", "<C-e>", function()
+      vim.keymap.set("i", "<tab>", function()
         return vim.fn["codeium#Accept"]()
       end, { expr = true })
       vim.keymap.set("i", "<c-;>", function()

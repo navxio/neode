0a6ebfd889e0320cc4bfa653c4f5a7fa3582da4b
Navdeep S <navdeep@mailbox.org>
2024-07-05 16:30:54 +0530

    enable codesnap for code screenshots

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "mistricky/codesnap.nvim",
+    build = "make",
+    config = function()
+      require("codesnap").setup({
+        has_breadcrumbs = true,
+        has_line_number = true,
+        bg_padding = 0,
+        watermark = "",
+        mac_window_bar = false,
+      })
+    end,
+  },
+}

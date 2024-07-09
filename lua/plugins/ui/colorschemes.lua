ec3b56ccf95329e0a591a65014efe78f4cfa5ecf
Nav Saini <navdeep@mailbox.org>
2024-07-09 22:42:09 +0530

    setup cyberdream theme options

--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -4,6 +4,12 @@ return {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        italic_comments = true,
+        transparent = true,
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",

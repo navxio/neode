4b0547c11339d6cee059a5c52784f642c68254e6
Nav Saini <navdeep@mailbox.org>
2024-07-09 22:39:13 +0530

    move to cyberdream colorscheme

--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -1,9 +1,14 @@
 return {
   { "dasupradyumna/midnight.nvim", lazy = false, priority = 1000 },
   {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "midnight",
+      colorscheme = "cyberdream",
     },
   },
 }

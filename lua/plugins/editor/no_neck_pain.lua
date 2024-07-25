0e6222591462dc778422ad6fd6d7c4d3454614ce
Navdeep Saini <navdeep@mailbox.org>
2024-07-25 20:15:43 +0530

    reconfigure no-neck-pain

--- lua/plugins/editor/no_neck_pain.lua
+++ lua/plugins/editor/no_neck_pain.lua
@@ -2,10 +2,26 @@ return {
   {
     "shortcuts/no-neck-pain.nvim",
     version = "*",
-    opts = {
-      autocmds = {
-        enableOnVimEnter = true,
-      },
-    },
+    lazy = false,
+    config = function()
+      require("no-neck-pain").setup({
+        autocmds = {
+          enableOnVimEnter = true,
+        },
+        integrations = {
+          NeoTree = {
+            position = "right",
+            reopen = true,
+          },
+          outline = {
+            position = "right",
+            reopen = true,
+          },
+        },
+        bufferOptions = {
+          enabled = false,
+        },
+      })
+    end,
   },
 }

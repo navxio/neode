be73a70f05e5d81a36a0e475dc2265275db7f282
Nav Saini <navdeep@mailbox.org>
2024-08-08 22:43:49 +0530

    folding: disable custom statuscol config for now

--- lua/plugins/editor/folding.lua
+++ lua/plugins/editor/folding.lua
@@ -48,28 +48,7 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = {
       "kevinhwang91/promise-async",
-      {
-        "luukvbaal/statuscol.nvim",
-        config = function()
-          local builtin = require("statuscol.builtin")
-          require("statuscol").setup({
-            -- configuration goes here, for example:
-            relculright = true,
-            segments = {
-              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-              {
-                sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
-                click = "v:lua.ScSa",
-              },
-              { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-              {
-                sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-                click = "v:lua.ScSa",
-              },
-            },
-          })
-        end,
-      },
+      "luukvbaal/statuscol.nvim",
     },
     lazy = true,
     event = "BufReadPost",

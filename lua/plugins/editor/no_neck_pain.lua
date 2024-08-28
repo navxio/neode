6d5edf2ecabb2ae0465ddcb9d075a0db7351d0ff
Navdeep Saini <navdeep@mailbox.org>
2024-08-28 14:57:26 +0530

    disable no neck pain on startup

--- lua/plugins/editor/no_neck_pain.lua
+++ lua/plugins/editor/no_neck_pain.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       require("no-neck-pain").setup({
         autocmds = {
-          enableOnVimEnter = true,
+          enableOnVimEnter = false,
           skipEnteringNoNeckPainBuffer = true,
         },
       })

c3e47b70c26510d12d925eba8e70fe6b31a4da1f
Navdeep Saini <navdeep@mailbox.org>
2024-10-23 21:07:57 +0530

    neotree: add copy filename shortcut

--- lua/plugins/custom.neo_tree.lua
+++ lua/plugins/custom.neo_tree.lua
@@ -9,6 +9,19 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     event = "VeryLazy",
     opts = {
+      filesystem = {
+        commands = {
+          copy_file_name = function(state)
+            local node = state.tree:get_node()
+            vim.fn.setreg("*", node.name, "c")
+          end,
+        },
+        window = {
+          mappings = {
+            ["Y"] = "copy_file_name",
+          },
+        },
+      },
       sources = {
         "filesystem",
         "netman.ui.neo-tree",

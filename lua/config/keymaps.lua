f33bf6f15efdfe835c89fd08d2905f4f64a91657
Navdeep Saini <navdeep@mailbox.org>
2024-10-27 16:11:31 +0530

    cleanup keybindings / introduce a new

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,58 +14,14 @@ map("n", "<c-G>", function()
   Util.terminal({ "lazygit" }, { esc_esc = false, ctrl_hjkl = false })
 end, { desc = "Lazygit (cwd)" })
 
-vim.keymap.set("n", "<c-x>", ":bd<cr>")
-
-_G._cycle_focus_terminal_buffers = function()
-  -- get all the current buffers
-  local bufs = vim.api.nvim_list_bufs()
-
-  -- get the current buffer
-  local current_buf = vim.api.nvim_get_current_buf()
-
-  if vim.api.nvim_buf_get_option(current_buf, "buftype") == "terminal" then
-    -- if it is a terminal buffer
-    -- check if there are any more terminal buffers
-    -- if there are then switch to the next one
-    -- if there are not then do nothing
-    local terminal_buffers = {}
-    for _, buf in ipairs(bufs) do
-      if vim.api.nvim_buf_get_option(buf, "buftype") == "terminal" then
-        table.insert(terminal_buffers, buf)
-      end
-    end
-
-    if #terminal_buffers > 1 then
-      local current_index = vim.fn.index(terminal_buffers, current_buf) + 1
-      local next_index = current_index + 1
-      if next_index > #terminal_buffers then
-        next_index = 1
-      end
-      vim.api.nvim_set_current_buf(terminal_buffers[next_index])
-    else
-      vim.notify("Already in the last terminal buffer", "info", { title = "LazyVim" })
-    end
-  else
-    -- if it is not a terminal buffer
-    -- check if there are any terminal buffers
-    -- if there are then switch to the first one
-    -- if there are not then create one and switch to it
-    local terminal_buffers = {}
-    for _, buf in ipairs(bufs) do
-      if vim.api.nvim_buf_get_option(buf, "buftype") == "terminal" then
-        table.insert(terminal_buffers, buf)
-      end
-    end
-    if #terminal_buffers > 0 then
-      vim.api.nvim_set_current_buf(terminal_buffers[1])
-    else
-      vim.cmd("terminal")
-    end
-  end
-end
+vim.keymap.set("n", "<c-x>", function()
+  vim.cmd.bd()
+end)
 
 -- open aerial
-vim.keymap.set("n", "<c-.>", "<cmd>Outline<cr>")
+vim.keymap.set("n", "<c-.>", function()
+  vim.cmd("Outline")
+end)
 
 -- open new terminal tab with tt
 vim.keymap.set("n", "tt", ":term<cr>")
@@ -78,19 +34,18 @@ vim.keymap.set("n", "<leader>ni", ":NeoAI<CR>")
 -- remap ; to : and vice versa in normal mode
 vim.keymap.set("n", ";", ":")
 
-vim.keymap.set("n", "<c-t>", "<cmd>lua _cycle_focus_terminal_buffers()<CR>")
-
 -- setup restnvim keybindings
 vim.keymap.set("n", "<leader>xr", "<Plug>RestNvim")
 vim.keymap.set("n", "<leader>xp", "<Plug>RestNvimPreview")
 vim.keymap.set("n", "<leader>xl", "<Plug>RestNvimLast")
 
 -- goto-preview
-vim.keymap.set("n", "gp", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { noremap = true })
+vim.keymap.set("n", "gp", function()
+  require("goto-preview").goto_preview_definition()
+end)
 
 -- off a random reddit thread
 vim.keymap.set("i", "<c-bs>", "<esc>cvb", {})
-
 -- comment-box related keybindings
 
 local opts = { noremap = true, silent = true }
@@ -114,7 +69,14 @@ vim.keymap.set({ "n", "v" }, "<Leader>np", "<Cmd>NoNeckPain<CR>", opts)
 vim.keymap.set({ "n", "v" }, "<Leader>]", ":Gen<CR>", opts)
 
 -- overseer commands
-vim.keymap.set({ "n", "v" }, "<leader>ox", "<cmd>OverseerRun<cr>", opts)
+-- override old c-o to c-a
+vim.keymap.set("n", "<C-a>", function()
+  vim.cmd("normal! <C-o>")
+end)
+--
+vim.keymap.set({ "n", "v" }, "<c-o>", function()
+  vim.cmd("OverseerRun")
+end, opts)
 vim.keymap.set({ "n", "v" }, "<leader>ot", "<cmd>OverseerToggle<cr>", opts)
 
 -- open config file

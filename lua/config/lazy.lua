c6a519d59c106f8d2a537b9de18c58ed7c6068a7
Navdeep Saini <navdeep@mailbox.org>
2024-11-09 20:17:49 +0530

    enable blink completion

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.octo" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.coding.blink" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.neogen" },
     -- import/override with your plugins

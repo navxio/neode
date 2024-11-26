8dbac4e6e10a0dc332625b3e98962a7ee9519b6c
Navdeep Saini <navdeep@mailbox.org>
2024-11-26 21:17:21 +0530

    remove commentbox plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,10 +18,6 @@ vim.keymap.set("n", "<c-x>", function()
   vim.cmd.bd()
 end)
 
-vim.keymap.set("n", "<esc><esc>", function()
-  vim.cmd.quit()
-end)
-
 -- open aerial
 vim.keymap.set("n", "<leader>.", function()
   vim.cmd("Outline")
@@ -48,24 +44,6 @@ vim.keymap.set("n", "gp", function()
   require("goto-preview").goto_preview_definition()
 end)
 
--- off a random reddit thread
-vim.keymap.set("i", "<c-bs>", "<esc>cvb", {})
--- comment-box related keybindings
-
-local opts = { noremap = true, silent = true }
-vim.keymap.set({ "n", "v" }, "<Leader>db", "<Cmd>CBccbox<CR>", opts)
--- Named parts
-vim.keymap.set({ "n", "v" }, "<Leader>dt", "<Cmd>CBllline<CR>", opts)
--- Simple line
---
-vim.keymap.set("n", "<Leader>dl", "<Cmd>CBline<CR>", opts)
--- keymap("i", "<M-l>", "<Cmd>CBline<CR>", opts) -- To use in Insert Mode
--- Marked comments
-vim.keymap.set({ "n", "v" }, "<Leader>dm", "<Cmd>CBllbox14<CR>", opts)
--- Removing a box is simple enough with the command (CBd), but if you
--- use it a lot:
-vim.keymap.set({ "n", "v" }, "<Leader>dd", "<Cmd>CBd<CR>", opts)
-
 -- no neck pain single keybinding
 vim.keymap.set({ "n", "v" }, "<Leader>np", "<Cmd>NoNeckPain<CR>", opts)
 
@@ -78,6 +56,8 @@ vim.api.nvim_set_keymap("n", "<c-a>", "<c-o>", { noremap = true, silent = true }
 vim.keymap.set({ "n", "v" }, "<c-o>", function()
   vim.cmd("OverseerRun")
 end, opts)
+
+--
 vim.keymap.set({ "n", "v" }, "<c-t>", function()
   vim.cmd("OverseerToggle")
 end, opts)
--- lua/plugins/tools.toggleterm.lua
+++ lua/plugins/tools.toggleterm.lua
@@ -10,10 +10,6 @@ return {
       require("toggleterm").setup({
         open_mapping = [[<c-`>]],
         direction = "float",
-        on_create = function()
-          -- source .venv/bin/activate only if it exists
-          vim.cmd("TermExec cmd='[ -f .venv/bin/activate ] && source .venv/bin/activate'")
-        end,
         start_in_insert = true,
         insert_mappings = true,
         close_on_exit = true,
